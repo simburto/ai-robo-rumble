8390d250102b8919bf193713f768232fcd545ea6
simburto <tingseamus@gmail.com>
2024-04-30 18:09:16 -0400

    angle
    fixed shitass angle detection
    ready for ai!!11!1

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def autoclimb(needlebboxcount):
     if needlebboxcount > 1:
         pyKey.press(key='r',sec=0.1)
 
+def info(info):
+    cv2.putText(frame, f"Angle: {info['intake_angle']} \n RobotPos: {info['robot_center']} \n BallCount: {info['balls_count']} \n ClimbCounter: {info['climb_counter']} \n CargoCount: {info['cargo_count']}", (50,100), font, fontScale, color, thickness, cv2.LINE_AA)
+
 for result in vision.vision():
     if result['climb_counter']:
         autoclimb(result['climb_counter'])
--- vision.py
+++ vision.py
@@ -9,6 +9,8 @@ def vision():
     fontScale = 1
     color = (255, 0, 0)  
     thickness = 2
+    cX = 0
+    cY = 0
 
     while True:
         result = {
@@ -17,122 +19,123 @@ def vision():
             'intake_angle': None,
             'balls_count': 0,
             'climb_counter': None,
+            'cargo_count': 0,
         }
-        while True:
-            img = pyautogui.screenshot()
+        img = pyautogui.screenshot()
 
-            frame = np.array(img)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        frame = np.array(img)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-            lower_red = np.array([1, 202, 236])
-            upper_red = np.array([3, 204, 238])
-            lower_bumper_red = np.array([0, 169, 167])
-            upper_bumper_red = np.array([37, 174, 220])
-            lower_nocargo = np.array([0,150,144])
-            upper_nocargo = np.array([1,152,146])
-            lower_ycargo = np.array([25,220,120])
-            upper_ycargo = np.array([35,230,130])
-            lower_intake = np.array([0, 0, 0]) 
-            upper_intake = np.array([1,1,1]) 
-            lower_climb = np.array([53,184,182]) 
-            upper_climb = np.array([55,186,184])
+        lower_red = np.array([1, 202, 236])
+        upper_red = np.array([3, 204, 238])
+        lower_bumper_red = np.array([0, 169, 167])
+        upper_bumper_red = np.array([37, 174, 220])
+        lower_nocargo = np.array([0,150,144])
+        upper_nocargo = np.array([1,152,146])
+        lower_ycargo = np.array([25,220,120])
+        upper_ycargo = np.array([35,230,130])
+        lower_intake = np.array([0, 0, 0]) 
+        upper_intake = np.array([1,1,1]) 
+        lower_climb = np.array([53,184,182]) 
+        upper_climb = np.array([55,186,184])
 
-            climb_mask = cv2.inRange(hsv, lower_climb, upper_climb)
-            intake_mask = cv2.inRange(hsv, lower_intake, upper_intake)
-            ycargo_mask = cv2.inRange(hsv, lower_ycargo, upper_ycargo)
-            nocargo_mask = cv2.inRange(hsv, lower_nocargo, upper_nocargo)
-            ball_mask = cv2.inRange(hsv, lower_red, upper_red)
-            bumper_mask = cv2.inRange(hsv, lower_bumper_red, upper_bumper_red)
-            kernel = np.ones((3, 3), np.uint8)
-            bumper_mask = cv2.erode(bumper_mask, kernel, iterations=1)
-            bumper_mask = cv2.dilate(bumper_mask, kernel, iterations=1)
-            climb_mask = cv2.erode(climb_mask, kernel, iterations=1)
-            climb_mask = cv2.dilate(climb_mask, kernel, iterations=1)
+        climb_mask = cv2.inRange(hsv, lower_climb, upper_climb)
+        intake_mask = cv2.inRange(hsv, lower_intake, upper_intake)
+        ycargo_mask = cv2.inRange(hsv, lower_ycargo, upper_ycargo)
+        nocargo_mask = cv2.inRange(hsv, lower_nocargo, upper_nocargo)
+        ball_mask = cv2.inRange(hsv, lower_red, upper_red)
+        bumper_mask = cv2.inRange(hsv, lower_bumper_red, upper_bumper_red)
+        kernel = np.ones((3, 3), np.uint8)
+        bumper_mask = cv2.erode(bumper_mask, kernel, iterations=1)
+        bumper_mask = cv2.dilate(bumper_mask, kernel, iterations=1)
+        climb_mask = cv2.erode(climb_mask, kernel, iterations=1)
+        climb_mask = cv2.dilate(climb_mask, kernel, iterations=1)
 
-            edges = cv2.Canny(ball_mask,100,200)
+        edges = cv2.Canny(ball_mask,100,200)
 
-            circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, dp=1, minDist=20, param1=50, param2=25, minRadius=0, maxRadius=35)
+        circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, dp=1, minDist=20, param1=50, param2=25, minRadius=0, maxRadius=35)
 
-            if circles is not None:
-                circles = np.round(circles[0, :]).astype("int")
-                result['balls_count'] = len(circles)
-                for (x, y, r) in circles:
-                    cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
-                    cv2.rectangle(frame, (x - r, y - r), (x + r, y + r), (0, 255, 0), 2)
-                    result['balls_coords'].append((x, y))
+        if circles is not None:
+            circles = np.round(circles[0, :]).astype("int")
+            result['balls_count'] = len(circles)
+            for (x, y, r) in circles:
+                cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
+                cv2.rectangle(frame, (x - r, y - r), (x + r, y + r), (0, 255, 0), 2)
+                result['balls_coords'].append((x, y))
 
-            contours, _ = cv2.findContours(bumper_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            if contours:
-                all_points = np.concatenate(contours)
+        contours, _ = cv2.findContours(bumper_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if contours:
+            all_points = np.concatenate(contours)
+            rect = cv2.minAreaRect(all_points)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)  
+            for i in box:
+                cv2.circle(frame,(i[0],i[1]), 10, (255,255,0), -1)
+            cv2.drawContours(frame, [box], 0, (255, 0, 0), 2)
+            M = cv2.moments(box)
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            cv2.circle(frame, (cX, cY), 5, (36, 255, 12), -1)
+            result['robot_center'] = (cX, cY)
+
+        contours, _ = cv2.findContours(intake_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if contours:
+            all_points = np.concatenate(contours)
+            if len(all_points) > 0:
                 rect = cv2.minAreaRect(all_points)
                 box = cv2.boxPoints(rect)
-                box = np.int0(box)  
-                for i in box:
-                    cv2.circle(frame,(i[0],i[1]), 10, (255,255,0), -1)
-                cv2.drawContours(frame, [box], 0, (255, 0, 0), 2)
-                M = cv2.moments(box)
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-                cv2.circle(frame, (cX, cY), 5, (36, 255, 12), -1)
-                result['robot_center'] = (cX, cY)
+                box = np.int0(box)
+                longest_side = max(np.linalg.norm(box[i] - box[(i + 1) % 4]) for i in range(4))
+                cv2.drawContours(frame, [box], 0, (0, 255, 0), 2)
 
-            contours, _ = cv2.findContours(intake_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            if contours:
-                all_points = np.concatenate(contours)
-                if len(all_points) > 0:
-                    rect = cv2.minAreaRect(all_points)
-                    box = cv2.boxPoints(rect)
-                    box = np.int0(box)
-                    longest_side = max(np.linalg.norm(box[i] - box[(i + 1) % 4]) for i in range(4))
-                    cv2.drawContours(frame, [box], 0, (0, 255, 0), 2)
+                for i in range(4):
+                    p1, p2 = box[i], box[(i + 1) % 4]
+                    side_length = np.linalg.norm(p1 - p2)
+                    if side_length == longest_side:
+                        cv2.line(frame, tuple(p1), tuple(p2), (0, 0, 255), 2)
+                        break
 
-                    for i in range(4):
-                        p1, p2 = box[i], box[(i + 1) % 4]
-                        side_length = np.linalg.norm(p1 - p2)
-                        if side_length == longest_side:
-                            cv2.line(frame, tuple(p1), tuple(p2), (0, 0, 255), 2)
-                            break
+        if contours:
+            bumper_center = (cX, cY)
 
-            if contours:
-                bumper_center = (cX, cY)
+            intake_longest_side = max(cv2.arcLength(contour, True) for contour in contours)
+            for contour in contours:
+                if cv2.arcLength(contour, True) == intake_longest_side:
+                    rect = cv2.minAreaRect(contour)
+                    intake_box = cv2.boxPoints(rect)
+                    intake_box = np.int0(intake_box)
+                    angle_rad = math.atan2(p2[1] - p1[1], p2[0] - p1[0])
+                    angle_deg = math.degrees(angle_rad) 
+                    result['intake_angle'] = angle_deg
+                    ray_length = 100 
+                    p2_x = int(cX + ray_length * math.cos(angle_rad))
+                    p2_y = int(cY + ray_length * math.sin(angle_rad))
+                    cv2.line(frame, bumper_center, (p2_x, p2_y), (50, 255, 255), 2)
 
-                intake_longest_side = max(cv2.arcLength(contour, True) for contour in contours)
-                for contour in contours:
-                    if cv2.arcLength(contour, True) == intake_longest_side:
-                        rect = cv2.minAreaRect(contour)
-                        intake_box = cv2.boxPoints(rect)
-                        intake_box = np.int0(intake_box)
-                        p1, p2 = intake_box[0], intake_box[1] 
-                        angle_rad = math.atan2(p2[1] - p1[1], p2[0] - p1[0])
-                        angle_deg = math.degrees(angle_rad)
-                        result['intake_angle'] = angle_deg
+        contours, _ = cv2.findContours(climb_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if contours:
+            climb_counter = 0
+            for cnt in contours:
+                x,y,w,h = cv2.boundingRect(cnt)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+                climb_counter += 1
+            result['climb_counter'] = climb_counter
 
-                        ray_length = 100 
-                        p2_x = int(cX + ray_length * math.cos(angle_rad))
-                        p2_y = int(cY + ray_length * math.sin(angle_rad))
-                        cv2.line(frame, bumper_center, (p2_x, p2_y), (0, 255, 255), 2)
+        if np.any(nocargo_mask != 0):
+            cv2.putText(frame, 'No cargo detected', org, font, fontScale, color, thickness, cv2.LINE_AA)
+            result['cargo_count'] = 0
+        elif np.any(ycargo_mask != 0):
+            cv2.putText(frame, '1 cargo', org, font, fontScale, color, thickness, cv2.LINE_AA)
+            result['cargo_count'] = 1
+        else:
+            cv2.putText(frame, '2 cargo', org, font, fontScale, color, thickness, cv2.LINE_AA)
+            result['cargo_count'] = 2
 
-            contours, _ = cv2.findContours(climb_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            if contours:
-                climb_counter = 0
-                for cnt in contours:
-                    x,y,w,h = cv2.boundingRect(cnt)
-                    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
-                    climb_counter += 1
-                result['climb_counter'] = climb_counter
+        result['frame'] = frame 
 
-            if np.any(nocargo_mask != 0):
-                cv2.putText(frame, 'No cargo detected', org, font, fontScale, color, thickness, cv2.LINE_AA)
-            elif np.any(ycargo_mask != 0):
-                cv2.putText(frame, '1 cargo', org, font, fontScale, color, thickness, cv2.LINE_AA)
-            else:
-                cv2.putText(frame, '2 cargo', org, font, fontScale, color, thickness, cv2.LINE_AA)
+        yield result
+        if cv2.waitKey(1) == ord('q'):
+            break
 
-            result['frame'] = frame 
-
-            yield result
-            if cv2.waitKey(1) == ord('q'):
-                break
-
-        cv2.destroyAllWindows()
+cv2.destroyAllWindows()
